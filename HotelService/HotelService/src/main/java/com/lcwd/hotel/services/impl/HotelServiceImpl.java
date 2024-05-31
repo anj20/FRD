cab48d6b91ebb8aa5888d3b171847874f6ec9d18
anujena <anurag.jena@oracle.com>
2024-05-31 13:07:05 +0530

    feat:reddisconfig[C

--- HotelService/HotelService/src/main/java/com/lcwd/hotel/services/impl/HotelServiceImpl.java
+++ HotelService/HotelService/src/main/java/com/lcwd/hotel/services/impl/HotelServiceImpl.java
@@ -4,6 +4,7 @@ import com.lcwd.hotel.entites.Hotel;
 import com.lcwd.hotel.exceptions.ResourceNotFoundException;
 import com.lcwd.hotel.respositories.HotelRepository;
 import com.lcwd.hotel.services.HotelService;
+import com.lcwd.hotel.services.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,8 @@ public class HotelServiceImpl implements HotelService {
 
     @Autowired
     private HotelRepository hotelRepository;
+    @Autowired
+    private RedisService redisService;
 
     @Override
     public Hotel create(Hotel hotel) {
@@ -30,6 +33,14 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public Hotel get(String id) {
-        return hotelRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("hotel with given id not found !!"));
+        Hotel resp = redisService.get(id,Hotel.class);
+        if (resp == null) {
+            Hotel body=hotelRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("hotel with given id not found !!"));
+            redisService.set(id,body,300l);
+            return body;
+        }
+        else {
+            return resp;
+        }
     }
 }
